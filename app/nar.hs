b694e893a95608ecd50afc31813f4c252a20f2f8
Erik de Castro Lopo <erikd@mega-nerd.com>
2021-08-17 18:13:57 +1000

    Remove redundant import

--- app/nar.hs
+++ app/nar.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Data.Maybe (fromMaybe)
-import           Data.Monoid ((<>))
 import           Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.Text.IO as Text
